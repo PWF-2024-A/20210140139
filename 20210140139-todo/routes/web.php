8e7b391bf06c33acedae90173211b3a457bd4c29
Sarah Hanna Taradipa <sarahhannata@gmail.com>
2024-03-22 16:33:59 +0700

    env, mvc, route, relationship

--- 20210140139-todo/routes/web.php
+++ 20210140139-todo/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +30,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
+
+Route::get('/user', [UserController::class, 'index'])->name('user.index');
+
+
+require __DIR__ . '/auth.php';
